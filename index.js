037f7215c070963b04acc9dc055b08881138091d
macsbook <maks.naumovich@gmail.com>
2024-01-31 00:21:11 +0100

    added acceleration function

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const button = document.getElementById('irritatingButton');
 
 const html = document.querySelector('html'); 
 
-html.addEventListener('mousemove', function (event) {
-  // добавляем обработчик события "mousemove"
-  const x = event.clientX; // получаем координату X мыши
-  const y = event.clientY; // получаем координату Y мыши
-
-  button.style.top = y + "px";
-  button.style.left = x + "px";
-  console.log(`Координаты мыши: x=${x}, y=${y}`); // выводим координаты мыши в консоль
-});
+let timerId = setTimeout(() => {
+  let speed = 80;
+  let timerId = setInterval(() => {
+    speed -= 0.1;
+    console.log(speed);
+    if (speed <= 0.3) {
+      clearInterval(timerId)
+      speed = 0.3;
+    }
+    button.style.setProperty('--button-speed', speed + 's');
+  }, 1000); 
+  html.addEventListener('mousemove', function (event) {
+    const x = event.clientX;
+    const y = event.clientY;
+  
+    button.style.top = y + "px";
+    button.style.left = x + "px";
+    console.log(`координаты: x:${x}, y:${y}`);
+  });
+}, 10000);
